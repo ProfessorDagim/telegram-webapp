3e506f7476289c445ebcb1590dcd504cf2b680b4
Dagim <dagimermiaskassa@gmail.com>
2025-08-01 12:18:25 +0300

    Added glowing effect

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
+// This PostCSS configuration file is set up to use Tailwind CSS and Autoprefixer.  
